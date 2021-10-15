570fc80385263903d0834f1c46c2680e10b01d51
Crowdbotics <team@crowdbotics.com>
2021-10-15 18:32:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "delay_31449",
+  url: "https://delay_31449.botics.co",
+  api: "https://delay_31449.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
